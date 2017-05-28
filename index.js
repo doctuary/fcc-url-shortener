6bdfb5cd2fd520b6d81bd4092d06cdfbb269308d
bloctuary <doctuary@outlook.com>
2017-05-28 08:02:49 -0500

    Add basic code for connecting to MongoDb

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 var express = require('express');
 var app = express();
 
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var url = 'mongodb://localhost:27017/my_database_name';      
+//(Focus on This Variable)
+
+// Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {    
