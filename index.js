6ca1426c0634ff66080e0f2f3f09726036a6bd3b
bloctuary <doctuary@github.com>
2017-06-01 06:55:32 -0500

    Use isUri to determine conditional path

--- index.js
+++ index.js
@@ -35,8 +35,17 @@ app.get('/', function(req, res) {
     res.send('This is a url shortening microservice. Try passing in <this url>/shrink/:originalUrl.');
 });
 app.get('/shrink/:sourceUrl*', function(req, res) {
-  var isUri = validUrl.isUri(req.params.sourceUrl);
-  res.send(isUri);
+  let sourceUrl = req.params.sourceUrl;
+  let isUri = validUrl.isUri(sourceUrl);
+  if (isUri) {
+    let map = {
+      longUrl: sourceUrl,
+      shortUrl: String.toString(Math.random() * 1000);
+    }
+    res.send(map);    
+  } else {
+    res.send(sourceUrl + " is not a valid URL.")
+  }
 });
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
